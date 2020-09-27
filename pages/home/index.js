2ef7078ee2b52524a50c652c2b2cb50758658b46
何秀钢 <xiugang.he@skyee360.com>
2020-09-27 17:10:07 +0800

    modified 每日答题

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,66 +1,66 @@
 // pages/home/index.js
-Page({
+const WXAPI = require('apifm-wxapi')
 
-  /**
-   * 页面的初始数据
-   */
+Component({
   data: {
-
+    answer_tips: '',
+    answer_list: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  lifetimes: {
+    /**
+     * 在组件实例刚刚被创建时执行
+     */
+    created: function () {
+      wx.showShareMenu({
+        withShareTicket: true
+      })
+    },
+    /**
+     * 在组件实例进入页面节点树时执行
+     */
+    attached: function () {
+      setTimeout(() => {
+        this.setData({
+          answer_tips: wx.getStorageSync('answer_tips')
+        })
+      }, 200)
+      this.getAnswerList()
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  methods: {
+    /**
+     * 用户点击右上角转发
+     */
+    onShareAppMessage: function () {
+      return {
+        title: wx.getStorageSync('share_title') || undefined
+      }
+    },
+    /**
+     * 监听用户下拉刷新事件
+     */
+    onPullDownRefresh: function () {
+      this.getAnswerList()
+    },
+    async getAnswerList() {
+      const params = {
+        page: 1,
+        pageSize: 20,
+        categoryId: wx.getStorageSync('answer_cid')
+      }
+      wx.showToast({
+        title: '数据加载中',
+        icon: 'loading'
+      })
+      const res = await WXAPI.cmsArticles(params)
+      console.log('res', res)
+      if (res.code === 0) {
+        this.setData({
+          answer_list: res.data
+        })
+      }
+      wx.stopPullDownRefresh()
+      wx.hideToast()
+    }
   }
 })
